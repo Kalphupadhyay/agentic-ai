1074c38e0c5b6295c1347acadf4b9e1a3c7013a6
Kalph upadhyay <kalpupadhyay@gmail.com>
2025-08-17 08:26:00 +0530

    first commit

--- src/constants/enum/Persona.ts
+++
@@ -1,4 +0,0 @@
-export enum Persona {
-  CHILL = "kalph-chill",
-  WORK = "kalph-work",
-}
--- src/constants/interface/chatRequest.ts
+++
@@ -1,6 +0,0 @@
-import type { Persona } from "../enum/Persona.js";
-
-export interface ChatRequest {
-  message: string;
-  persona: Persona;
-}
